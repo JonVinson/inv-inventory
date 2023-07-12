ad193a217d97b06bbe9e1ecc4d37992f263c758e
Jon Vinson <jdv@jdvinson.com>
2023-07-11 20:56:43 -0400

    INV-12 Supplier Page

--- inv-service/InventoryService.cs
+++ inv-service/InventoryService.cs
@@ -12,6 +12,7 @@ namespace inv_service
             _context = context;
         }
 
+        #region Departments
         public IQueryable<Department> GetDepartments()
         {
             return _context.Departments;
@@ -27,14 +28,14 @@ namespace inv_service
 
             _context.Departments.Add(department);
             _context.SaveChanges();
-            
+
             return department.Id;
         }
 
         public void DeleteDepartment(int id)
         {
             var department = _context.Departments.FirstOrDefault(p => p.Id == id);
-            
+
             if (department != null)
             {
                 _context.Departments.Remove(department);
@@ -54,5 +55,100 @@ namespace inv_service
                 _context.SaveChanges();
             }
         }
+        #endregion
+
+        #region Suppliers
+        public IQueryable<Supplier> GetSuppliers()
+        {
+            return _context.Suppliers;
+        }
+
+        public int CreateSupplier(Supplier model)
+        {
+            _context.Suppliers.Add(model);
+            _context.SaveChanges();
+
+            return model.Id;
+        }
+
+        public void DeleteSupplier(int id)
+        {
+            var supplier = _context.Suppliers.FirstOrDefault(p => p.Id == id);
+
+            if (supplier != null)
+            {
+                _context.Suppliers.Remove(supplier);
+            }
+
+            _context.SaveChanges();
+        }
+
+        public void UpdateSupplier(Supplier model)
+        {
+            var supplier = _context.Suppliers.FirstOrDefault(p => p.Id == model.Id);
+
+            if (supplier != null)
+            {
+                supplier.Name = model.Name;
+                supplier.Code = model.Code;
+                supplier.Street = model.Street;
+                supplier.City = model.City;
+                supplier.State = model.State;
+                supplier.Country = model.Country;
+                supplier.PostalCode = model.PostalCode;
+                supplier.ContactEmail = model.ContactEmail;
+                supplier.ContactName = model.ContactName;
+                supplier.PhoneNumber = model.PhoneNumber;
+                _context.SaveChanges();
+            }
+        }
+        #endregion
+
+        #region Customers
+        public IQueryable<Customer> GetCustomers()
+        {
+            return _context.Customers;
+        }
+
+        public int CreateCustomer(Customer model)
+        {
+            _context.Customers.Add(model);
+            _context.SaveChanges();
+
+            return model.Id;
+        }
+
+        public void DeleteCustomer(int id)
+        {
+            var customer = _context.Customers.FirstOrDefault(p => p.Id == id);
+
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+            }
+
+            _context.SaveChanges();
+        }
+
+        public void UpdateCustomer(Customer model)
+        {
+            var customer = _context.Customers.FirstOrDefault(p => p.Id == model.Id);
+
+            if (customer != null)
+            {
+                customer.Name = model.Name;
+                customer.Code = model.Code;
+                customer.Street = model.Street;
+                customer.City = model.City;
+                customer.State = model.State;
+                customer.Country = model.Country;
+                customer.PostalCode = model.PostalCode;
+                customer.ContactEmail = model.ContactEmail;
+                customer.ContactName = model.ContactName;
+                customer.PhoneNumber = model.PhoneNumber;
+                _context.SaveChanges();
+            }
+        }
+        #endregion
     }
 }
