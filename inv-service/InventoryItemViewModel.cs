348616c7413753a367febcb958bfaf9261e32897
Jon Vinson <jdv@jdvinson.com>
2023-07-14 13:38:24 -0400

    INV-15 Invertory history page

---
+++ inv-service/InventoryItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿using inventory_data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inv_service
+{
+    public class InventoryItemViewModel
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        public string? Description { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime AsOfDate { get; set; }
+    }
+}
---
+++ inventory-app/Controllers/InventoryController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Xml.Linq;
+using System.Web;
+
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using inv_service;
+using inventory_app.Models;
+using inventory_data;
+
+namespace inventory_app.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private readonly ILogger<InventoryController> _logger;
+        private readonly InventoryService _inventoryService;
+
+        public InventoryController(ILogger<InventoryController> logger, InventoryService inventoryService)
+        {
+            _logger = logger;
+            _inventoryService = inventoryService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Get()
+        {
+            try
+            {
+                var items = _inventoryService.GetInventoryItems();
+                return Json(items);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error reading items");
+                throw;
+            }
+        }
+
+        [AcceptVerbs("POST")]
+        public JsonResult Delete(int id)
+        {
+            try
+            {
+                _inventoryService.DeleteInventoryItem(id);
+                return Json(id);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error deleting item {id}");
+                throw;
+            }
+        }
+
+        [AcceptVerbs("POST")]
+        public JsonResult Update(InventoryItemViewModel model)
+        {
+            try
+            {
+                _inventoryService.UpdateInventoryItem(model);
+                return Json(model.Id);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error updating item {model.Id}");
+                throw;
+            }
+        }
+
+        [AcceptVerbs("POST")]
+        public JsonResult Create(InventoryItemViewModel model)
+        {
+            try
+            {
+                int id = _inventoryService.CreateInventoryItem(model);
+                return Json(id);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error creating item {model.Description}");
+                throw;
+            }
+        }
+
+        public JsonResult GetProducts()
+        {
+            try
+            {
+                var products = _inventoryService.GetProducts()
+                    .Select(p => new
+                    {
+                        Value = p.Id,
+                        Text = p.DepartmentCode + " - " + p.ManufacturerCode
+                            + " - " + p.ModelNumber + " - " + p.Description
+                    });
+                return Json(products);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error reading items");
+                throw;
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
